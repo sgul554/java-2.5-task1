81bf122ff4b87d8f5a779079ca31183a2c7acc0b
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-03-07 14:44:04 +0300

    Изменение кода для полного покрытия тестами

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,8 +10,8 @@ class StatisticsServiceTest {
   void findMax() {
     StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 13, 14, 150};
+    long expected = 150;
 
     long actual = service.findMax(incomesInBillions);
 
